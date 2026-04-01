2b9b119a208bed8f7982ba0cb784fd1f46d43894
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-04-02 01:00:31 +0200

    App started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,30 +4,57 @@ import Header from './Header';
 
 function App() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      fontFamily : 'sans-serif',
-      marginTop : '50px'
-    }}>
-      <header>
-        <nav>
-          <a href="/">Home</a>
-          <a href="/leaderboard">Leaderboard</a>
-          <a href="/profile">profile</a>
-        </nav>
-      </header>
-      <main className="main-board" style={{
-        display : 'flex',
-        flexDirection : 'column',
-        alignItems : 'center',
+    <div style={{display : 'flex', flexDirection : 'column', minHeight : '100vh'}}> 
+      <header style={{backgroundColor : '#1F1E1F', paddingTop : '15px', paddingBottom : '25px', 
+        borderTopWidth: '2px',
+        borderBottomWidth: '2px',
+        borderColor : 'white',
+        borderTopStyle : 'solid',
+        borderBottomStyle : 'solid',
       }}>
         <h1>Multiplayer Minesweeper</h1>
-        <Menu />
-        <Header /> 
-        <Board rows={10} cols={10}/> 
-      </main>
+      </header>
+
+      <div style={{display : 'flex', flex : 1, flexDirection : 'row', minHeight : '90vh'}}> 
+        <div style={{width : '200px', color : 'white', backgroundColor : '#333', borderRightStyle : 'solid', borderRightWidth : '1px', minHeight : '90vh'}}>
+          <aside >
+            <nav> 
+              <ul style={{margin : 0, padding : 0, listStyleType : 'none'}}> 
+                <li>Home</li>
+                <li>Leaderboard</li>
+                <li>Multiplayer</li>
+                <p>Test</p>
+              </ul>
+            </nav> 
+          </aside>
+        </div>
+
+        <div style={{
+          display: 'flex',
+          flex : 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontFamily : 'sans-serif',
+          marginTop : '50px',
+        }}>
+
+          <main className="main-board" style={{
+            display : 'flex',
+            flexDirection : 'column',
+            alignItems : 'center',
+            justifyContent : 'center'
+          }}>
+            <Menu />
+            <Header /> 
+            <Board rows={10} cols={10}/> 
+          </main>
+        </div>
+      </div>
+
+      <footer style={{backgroundColor : '#1F1E1F', borderTopStyle : 'solid', borderTopWidth : '2px', display:'flex', alignItems : 'right', justifyContent: 'right', flex : 1 }}> 
+        <a href='./privacy'>Privacy</a>
+        <a href='./support' style={{marginRight : '20px', marginLeft : '20px'}}>Support</a>
+      </footer>
     </div>
   );
 }
