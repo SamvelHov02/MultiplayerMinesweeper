0e1b944ea38d37330dee4e0c4d358abc5369a716
Samvel Hovhannisyan <samnar2002@icloud.com>
2026-04-04 02:08:23 +0200

    Home Complete

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,6 +3,8 @@ import settings from './settings.svg';
 import stats from './stats.svg';
 import swords from './swords.svg';
 import account from './square-user-round.svg';
+import single from './singleplayer.svg';
+import multi from './user-group.svg';
 
-export const Icons = {cog, settings, stats, swords, account};
+export const Icons = {cog, settings, stats, swords, account, single, multi};
 
